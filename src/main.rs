5837c1b6084907f6ba5542a73411e9ad03fc378e
dgarc359 <dgarc359@outlook.com>
2022-10-09 23:03:54 -0400

    refactor WrapperClient to impl new fn

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,26 @@
 use aws_config::{meta::region::RegionProviderChain, SdkConfig};
 use aws_sdk_ssm::{Client, Error};
 
-async fn build_ssm_client() -> SsmWrapperClient {
-    let region_provider: RegionProviderChain = RegionProviderChain::default_provider().or_else("us-east-1");
-
-    let config: SdkConfig = aws_config::from_env().region(region_provider).load().await;
-
-    SsmWrapperClient {
-        client: Client::new(&config) 
-    }
-}
-
 struct SsmWrapperClient {
     client: Client,
 }
 
+impl SsmWrapperClient {
+    async fn new() -> Self {
+            let region_provider: RegionProviderChain = RegionProviderChain::default_provider().or_else("us-east-1");
+
+        let config: SdkConfig = aws_config::from_env().region(region_provider).load().await;
+
+        Self {
+            client: Client::new(&config) 
+        }
+    }
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    let client = build_ssm_client().await.client;
+    // let client = build_ssm_client().await.client;
+    let client = SsmWrapperClient::new().await.client;
 
     let resp = client
         .get_parameters()
